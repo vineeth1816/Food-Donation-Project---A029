16048f83443fb719f818eff5a7168e9e3e62e4e6
vineeth1816 <vineeth1816@gmail.com>
2021-04-15 16:57:54 +0530

    Project Creation

---
+++ FD-A029/src/main/java/com/FD/FdA029Application.java
@@ -0,0 +1,13 @@
+package com.FD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FdA029Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FdA029Application.class, args);
+	}
+
+}
---
+++ FD-A029/src/test/java/com/FD/FdA029ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FdA029ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
